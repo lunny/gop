ae1c9210f2b555de951c74001a673d68846f1112
Lunny Xiao <xiaolunwen@gmail.com>
2017-06-27 15:43:37 +0800

    add yml on init

--- cmd/init.go
+++ cmd/init.go
@@ -34,8 +34,30 @@ func runInit(ctx *cli.Context) error {
 	os.MkdirAll("src", os.ModePerm)
 	os.MkdirAll("bin", os.ModePerm)
 
-	mainFile := filepath.Join("src", "main.go")
-	_, err := os.Stat(mainFile)
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	ymlPath := filepath.Join(wd, "gop.yml")
+	_, err = os.Stat(ymlPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			y, err := os.Create(ymlPath)
+			if err != nil {
+				return err
+			}
+			defer y.Close()
+
+			_, err = y.Write([]byte(fmt.Sprintf("name: %s\n", filepath.Base(wd))))
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	mainFile := filepath.Join(wd, "src", "main.go")
+	_, err = os.Stat(mainFile)
 	if err != nil {
 		if os.IsNotExist(err) {
 			f, err := os.Create(mainFile)
